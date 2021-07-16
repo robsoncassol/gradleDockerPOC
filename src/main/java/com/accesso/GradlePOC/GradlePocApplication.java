988be239f910a91f975ef86e1f2ab0439c879dda
robsoncassol <robsoncassol@gmail.com>
2021-07-16 16:11:39 -0300

    first commit

---
+++ src/main/java/com/accesso/GradlePOC/GradlePocApplication.java
@@ -0,0 +1,13 @@
+package com.accesso.GradlePOC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradlePocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradlePocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/accesso/GradlePOC/GradlePocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accesso.GradlePOC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradlePocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
