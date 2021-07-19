976f594b0f5fc974ee498b8cb8396e65c71f924b
robsoncassol <robsoncassol@gmail.com>
2021-07-19 13:52:15 -0300

    add git version plugin

---
+++ src/main/java/com/accesso/GradlePOC/GradlePocController.java
@@ -0,0 +1,16 @@
+package com.accesso.GradlePOC;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class GradlePocController {
+
+    @GetMapping("hello")
+    public String helloWorld(){
+        return "Hello world";
+
+    }
+
+}
